49233cda74b9a80a37e7b287104bf3574981c773
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-11-11 23:49:09 +0200

    C# 9 features

--- A2v10.Workflow.Tests/Simple/SimpleFlow.cs
+++ A2v10.Workflow.Tests/Simple/SimpleFlow.cs
@@ -19,13 +19,11 @@ namespace A2v10.Workflow.Tests
 			var root = new Sequence()
 			{
 				Ref = "Ref0",
-				Variables = new List<IVariable>
-				{
+				Variables = {
 					new Variable() {Name = "X", Dir = VariableDirection.In, Type=VariableType.Number},
 					new Variable() {Name = "R", Dir = VariableDirection.Out, Type=VariableType.Number}
 				},
-				Activities = new List<IActivity>()
-				{
+				Activities = {
 					new Code() {Ref="Ref1", Script="X = X + 1"},
 					new Code() {Ref="Ref2", Script="X = X + 1"},
 					new Code() {Ref="Ref3", Script="R = X"},
@@ -52,8 +50,7 @@ namespace A2v10.Workflow.Tests
 			var root = new Parallel()
 			{
 				Ref = "Ref0",
-				Branches = new List<IActivity>()
-				{
+				Branches = {
 					new Code() {Ref="Ref1", Script="console.log('ref1')"},
 					new Code() {Ref="Ref2", Script="console.log('ref2')"},
 					new Code() {Ref="Ref3", Script="console.log('ref3')"},
@@ -71,13 +68,11 @@ namespace A2v10.Workflow.Tests
 			var root = new Sequence()
 			{
 				Ref = "Ref0",
-				Variables = new List<IVariable>
-				{
+				Variables = {
 					new Variable() {Name = "X", Dir = VariableDirection.In, Type=VariableType.Number},
 					new Variable() {Name = "R", Dir = VariableDirection.Out, Type=VariableType.String}
 				},
-				Activities = new List<IActivity>()
-				{
+				Activities = {
 					new If() {
 						Ref="Ref1",
 						Condition="X > 5",
@@ -103,18 +98,15 @@ namespace A2v10.Workflow.Tests
 			var root = new Sequence()
 			{
 				Ref = "Ref0",
-				Variables = new List<IVariable>
-				{
+				Variables = {
 					new Variable() {Name = "X", Dir = VariableDirection.In, Type=VariableType.Number},
 					new Variable() {Name = "R", Dir = VariableDirection.Out, Type=VariableType.Number},
 				},
-				Activities = new List<IActivity>()
-				{
+				Activities = {
 					new Code() {Ref="Ref1", Script="X = X + 1"},
 					new Code() {Ref="Ref2", Script="X = X + 1"},
 					new Sequence() { Ref="Ref3",
-						Activities = new List<IActivity>()
-						{
+						Activities = {
 							new Code() {Ref="Ref4", Script="X = X + 1"},
 							new Code() {Ref="Ref5", Script="X = X + 1"},
 						}
