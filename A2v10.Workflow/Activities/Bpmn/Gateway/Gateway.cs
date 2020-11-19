f856dbc2a39f8d74875f6869fbb42ce90fd0e875
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-11-20 00:31:30 +0200

    gateway

--- A2v10.Workflow/Activities/Bpmn/Gateway/Gateway.cs
+++ A2v10.Workflow/Activities/Bpmn/Gateway/Gateway.cs
@@ -9,5 +9,8 @@ namespace A2v10.Workflow.Bpmn
 		public List<String> Outgoing { get; init; }
 
 		public String Default { get; init; }
+
+		public Boolean HasIncoming => Incoming != null && Incoming.Count > 0;
+		public Boolean HasOutgoing => Outgoing != null && Outgoing.Count > 0;
 	}
 }
--- A2v10.Workflow/Activities/Bpmn/Gateway/ParallelGateway.cs
+++ A2v10.Workflow/Activities/Bpmn/Gateway/ParallelGateway.cs
@@ -1,5 +1,6 @@
 ﻿using A2v10.Workflow.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace A2v10.Workflow.Bpmn
@@ -8,9 +9,31 @@ namespace A2v10.Workflow.Bpmn
 
 	public class ParallelGateway : Gateway
 	{
+		private readonly List<IToken> _tokens = new List<IToken>();
+
 		public override ValueTask ExecuteAsync(IExecutionContext context, IToken token, ExecutingAction onComplete)
 		{
-			throw new NotImplementedException();
+			// waits for all incoming tokens
+			_tokens.Add(token);
+			if (HasIncoming && _tokens.Count == Incoming.Count)
+				return DoOutgoing(context, onComplete);
+			else
+				return new ValueTask();
+		}
+
+		public ValueTask DoOutgoing(IExecutionContext context, ExecutingAction onComplete)
+		{
+			// kill all tokens
+			foreach (var t in _tokens)
+				Parent.KillToken(t);
+			if (HasOutgoing)
+			{
+				foreach (var og in Outgoing) {
+					var flow = Parent.FindElement<SequenceFlow>(og);
+					context.Schedule(flow, onComplete, Parent.NewToken());
+				}
+			}
+			return new ValueTask();
 		}
 	}
 }
