b4bcf1708d2cc577d1f723674d074955d8aba92f
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-11-26 12:16:53 +0200

    activityTask

---
+++ A2v10.Workflow/Activities/Bpmn/Task/ActivityTask.cs
@@ -0,0 +1,14 @@
+﻿using A2v10.Workflow.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2v10.Workflow.Bpmn
+{
+	public class ActivityTask : BpmnTask
+	{
+		IActivity Activity { get; init; }
+	}
+}
